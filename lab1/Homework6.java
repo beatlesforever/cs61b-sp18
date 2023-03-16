53055742dcd848c8009c356f1f9f0a5f00f420f3
zhouhaoran <994149252@qq.com>
2023-03-16 18:12:36 +0800

    upload hw1

---
+++ lab1/Homework6.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * @author zhouhaoran
+ * @date 2023/2/26
+ * @project study
+ */
+public class Homework6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter weight in pounds:");
+        double pounds = input.nextDouble();
+        System.out.print("Enter feet:");
+        double feet = input.nextDouble();
+        System.out.print("Enter inches:");
+        double inches = input.nextDouble();
+        final double KILOS_PER_POUND = 0.45359237;
+        //要注意常量的命名规范
+        final double METERS_PER_INCH = 0.0254;
+        final double METERS_PER_FOOT = 0.3048;
+        double kilos = pounds * KILOS_PER_POUND;
+        double meters = feet * METERS_PER_FOOT + inches * METERS_PER_INCH;
+        double bmi = kilos / (meters * meters);
+        System.out.println("BMI is "+bmi);
+        if(bmi < 18.5)
+            System.out.println("Underweight");
+        else if(bmi < 25)
+            System.out.println("Normal");
+        else if(bmi < 30)
+            System.out.println("Overweight");
+        else
+            System.out.println("Obese");
+
+    }
+}
